9464aad164e2de8edab1e8f362c87c69d9d589dd
alghaffri <121156756+alghaffri@users.noreply.github.com>
2023-01-23 12:29:07 +0400

    Update on Item class & invoice class

--- src/applicationSystem/Item.java
+++ src/applicationSystem/Item.java
@@ -2,10 +2,69 @@ package applicationSystem;
 
 public class Item {
 
-	String itemName;
-	int  itemId;
-	int unitPrice;
-	int quantity;
-	int quantityPrice;
+	private String itemName;
+	private double  itemId;
+	private double unitPrice;
+	private double quantity;
+	private double quantityPrice;
+
 	
+	
+	   public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public double getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(double itemId) {
+		this.itemId = itemId;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getQuantityPrice() {
+		return quantityPrice;
+	}
+
+	public void setQuantityPrice(double quantityPrice) {
+		this.quantityPrice = quantityPrice;
+	}
+
+	public static void displayFormat()   
+       {  
+           System.out.format("-----------------------------------------------------------------------------------------------------------------------------------");  
+           System.out.print("\n itemName \t \titemId \t \tunitPrice \t \t quantity \t \t quantityPrice \n");  
+           System.out.format("-----------------------------------------------------------------------------------------------------------------------------------\n");  
+       }  
+          
+       // display 
+	   
+       public void display()   
+       {  
+         
+		System.out.format("" ,itemName, itemId, unitPrice, quantity, quantityPrice);  
+     
+	
+	
+}
 }
--- src/applicationSystem/invoice.java
+++ src/applicationSystem/invoice.java
@@ -5,13 +5,18 @@ public class invoice {
 	String customerName;
 	int customerphonenumber;
 	String  invoiceDate;
-	int numberOfItems;
-	int totalAmount;
-	int paidAmount;
-	int balance;
+	 double numberOfItems;
+	 double totalAmount;
+	 double paidAmount;
+	 double balance;
 	
 public void SetinvoiceHeader() {
-
+	 System.out.println("\t\t\t\t--------------------Invoice-----------------");  
+     System.out.println("\t\t\t\t\t "+"  "+"Metro Mart Grocery Shop");  
+     System.out.println("\t\t\t\t\t carrefour");   
+     System.out.println("Fax : 564365"+" \t\t\t\t\t\t\t Tel : (+968) 96677030");  
+     System.out.println("\t\t\t\t\t"+" Email : Al@icloud.com "); 
+     System.out.println("\t\t\t\t\t"+" Website: www.shop.com "); 
 }
 
 
